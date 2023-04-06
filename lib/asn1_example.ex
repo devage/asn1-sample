27f1c1f87a831e7e15d7d6021704bd4a831fc487
ikpark <ikpark@gmail.com>
2023-04-06 13:38:25 +0900

    mix format

--- lib/asn1_example.ex
+++ lib/asn1_example.ex
@@ -2,7 +2,7 @@ defmodule Asn1Example do
   require :MyModule
 
   def encode_my_sequence(id, name) do
-    my_sequence = {:MySequence, id,  name}
+    my_sequence = {:MySequence, id, name}
 
     case :MyModule.encode(:MySequence, my_sequence) do
       {:ok, encoded_data} -> IO.inspect(:binary.bin_to_list(encoded_data))
